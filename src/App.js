72bd3afae0f19829e79a6bc3236e4474f1670240
oliverochman <oliver.ochman@hotmail.com>
2019-10-15 11:21:25 +0200

    add sematic ui and BMI converter header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
 import DisplayResult from './Components/displayResult';
+import {
+  Container,
+  Grid,
+  Header
+} from 'semantic-ui-react'
 
 class App extends Component {
   constructor(props) {
@@ -12,22 +17,34 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-        <div>
-          <label>Weight(kg)</label>
-          <input name="weight" value={this.state.weight} onChange={ (e) => this.setState({ weight: e.target.value })} />
-        </div>
+      <Container>
+        <Grid centered columns={3}>
+          <Grid.Column>
+            <Header
+              as="h1"
+              textAlign="center"
+            >
+              BMI Converter
+            </Header>
 
-        <div>
-          <label>Height(cm)</label>
-          <input name="height" value={this.state.height} onChange={ (e) => this.setState({ height: e.target.value })} />
-        </div>
+            <div>
+              <label>Weight(kg)</label>
+              <input name="weight" value={this.state.weight} onChange={ (e) => this.setState({ weight: e.target.value })} />
+            </div>
 
-        <DisplayResult
-          weight={this.state.weight}
-          height={this.state.height}
-        />
-      </div>
+            <div>
+              <label>Height(cm)</label>
+              <input name="height" value={this.state.height} onChange={ (e) => this.setState({ height: e.target.value })} />
+            </div>
+
+            <DisplayResult
+              weight={this.state.weight}
+              height={this.state.height}
+            />
+          </Grid.Column>
+          
+        </Grid>   
+      </Container>
     );
   }
 }
